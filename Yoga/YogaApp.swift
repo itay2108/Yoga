4999d49670f47ce7b33a8615b0e5f5cd8150f26d
itay2108 <itay2108@gmail.com>
2025-07-16 16:09:07 +0300

    Initial Commit

---
+++ Yoga/YogaApp.swift
@@ -0,0 +1,17 @@
+//
+//  YogaApp.swift
+//  Yoga
+//
+//  Created by Itay Gervash on 16/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct YogaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
